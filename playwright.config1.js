8a1d48c99e83be20c2337b35d7462e71d1a749d3
Htin Z <htindev@gmail.com>
2026-02-05 14:33:51 +0000

    added playwright.config1.js

---
+++ playwright.config1.js
@@ -0,0 +1,40 @@
+// @ts-check
+import { defineConfig, devices } from "@playwright/test";
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+
+const config = {
+  testDir: "./tests",
+  timeout: 30 * 1000,
+  expect: { timeout: 5000 },
+  reporter: "html",
+  projects: [
+    {
+      name: "safari",
+      use: {
+        browserName: "webkit",
+        headless: true,
+        screenshot: "off",
+        trace: "on",
+        // ...devices['iPhone 11'],
+      },
+    },
+    {
+      name: "chrome",
+      use: {
+        browserName: "chromium",
+        headless: false,
+        screenshot: "on",
+        // video: "retain-on-failure",
+        // ignoreHttpsErrors: true,
+        // permissons: ['geolocation'],
+        trace: "on",
+        // ...devices['iPhone 11'],
+        // viewport: {width:720, height: 720},
+      },
+    },
+  ],
+};
+
+module.exports = config;
