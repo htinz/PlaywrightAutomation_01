91046dbfb2c08e95abf96398377e4617507dba86
Htin Z <htindev@gmail.com>
2026-02-07 21:58:47 +0000

    Added TypeScript file demo.ts

---
+++ tests/demo.js
@@ -0,0 +1,8 @@
+let message = "Hello";
+console.log(message);
+message = 2;
+console.log(message);
+let age = 20;
+console.log(age);
+let number = [1, 2, 3, 4];
+console.log(number[3]);
---
+++ tests/demo1.ts
@@ -0,0 +1,14 @@
+let message1: string = "Hello";
+message1 = "bye";
+console.log(message1);
+
+let age1: number = 20;
+console.log(age1);
+let isActive: boolean = false;
+console.log(isActive);
+let numberArray: number[] = [1, 2, 3];
+console.log(numberArray[0]);
+let data: any = "This could be anything";
+console.log(data);
+data = 42;
+console.log(data);
